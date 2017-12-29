340b7db811123184daf331ddd80054943a60d5ce
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2017-12-29 08:50:50 +0100

    Added a function and an aggregate to fill gaps in window functions.

---
+++ gab_fill.sql
@@ -0,0 +1,18 @@
+-- Create a window function to calculate values for gaps
+CREATE OR REPLACE FUNCTION GapFillInternal(s anyelement, v anyelement)
+RETURNS anyelement AS
+$$
+BEGIN
+	RETURN COALESCE(v, s);
+END;
+$$ LANGUAGE PLPGSQL IMMUTABLE;
+COMMENT ON FUNCTION GapFillInternal(s anyelement, v anyelement) IS 'The function is used to fill gaps in window functions';
+
+
+-- The Window function needs an aggregate
+CREATE AGGREGATE GapFill(anyelement) (
+	SFUNC=GapFillInternal,
+	STYPE = anyelement
+)
+;
+COMMENT ON AGGREGATE GapFill(anyelement) IS 'Implements the aggregate function to fill gaps using the function GapFillInternal';
