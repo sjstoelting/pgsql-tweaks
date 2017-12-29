b5018f4068717e3596d2a4c51fff696391390ad4
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2017-12-29 08:44:03 +0100

    Added a function to validate strings containing only latin1 characters.

---
+++ function_is_latin1.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION is_latin1(s VARCHAR) RETURNS BOOLEAN AS $$
+BEGIN
+    PERFORM convert(s::bytea, 'UTF8', 'LATIN1');
+    RETURN TRUE;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION is_latin1(s VARCHAR) IS 'Checks, whether the given parameter contains only latin1 characters';
