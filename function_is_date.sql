a43ca868bc4098cf22fba9c7313c68ca8008feb1
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2017-12-29 08:43:16 +0100

    Added a second function to check if a text is a date by a format string as parameter.

--- function_is_date.sql
+++ function_is_date.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.is_date(s VARCHAR) RETURNS BOOLEAN AS $$
+CREATE OR REPLACE FUNCTION is_date(s VARCHAR) RETURNS BOOLEAN AS $$
 BEGIN
   PERFORM s::date;
   RETURN TRUE;
@@ -9,3 +9,18 @@ $$
 STRICT
 LANGUAGE plpgsql IMMUTABLE
 ;
+COMMENT ON FUNCTION is_date(s VARCHAR) IS 'Takes a varchar and checks if it is a date, uses standard date format YYYY-MM-DD';
+
+
+CREATE OR REPLACE FUNCTION is_date(s VARCHAR, f VARCHAR) RETURNS BOOLEAN AS $$
+BEGIN
+  PERFORM to_date(s, f);
+  RETURN TRUE;
+EXCEPTION WHEN OTHERS THEN
+  RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION is_date(s VARCHAR, f VARCHAR) IS 'Takes a varchar and checks if it is a date by taking the second varchar as date format';
