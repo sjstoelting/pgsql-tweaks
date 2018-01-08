328202c98cff6c92977a83b9ad453a43d05eaef1
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-08 22:09:23 +0100

    Added a function to check strings for being timestamps with some tests.

--- function_is_timestamp.sql
+++ function_is_timestamp.sql
@@ -1,3 +1,8 @@
+/**
+ * Creates two functions to check strings for being timestamps.
+ * The first function checks it with the default format, the second with the
+ * format given as parameter.
+ */
 CREATE OR REPLACE FUNCTION is_timestamp(s VARCHAR) RETURNS BOOLEAN AS $$
 BEGIN
     PERFORM s::TIMESTAMP;
@@ -7,4 +12,18 @@ EXCEPTION WHEN others THEN
 END;
 $$
 STRICT
-LANGUAGE plpgsql IMMUTABLE
+LANGUAGE plpgsql IMMUTABLE;
+COMMENT ON FUNCTION is_timestamp(s VARCHAR) IS 'Takes a varchar and checks if it is a timestamp without time zone, uses standard timestamp format YYYY-MM-DD HH24:MI:SS';
+
+
+CREATE OR REPLACE FUNCTION is_timestamp(s VARCHAR, f VARCHAR) RETURNS BOOLEAN AS $$
+BEGIN
+    PERFORM to_timestamp(s, f)::TIMESTAMP;
+    RETURN TRUE;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE;
+COMMENT ON FUNCTION is_timestamp(s VARCHAR) IS 'Takes a varchar and checks if it is a timestamp by taking the second varchar as date format';
---
+++ tests/function_is_timestamp.sql
@@ -0,0 +1,79 @@
+/**
+ * Test for function is_timestamp
+ *
+ * Every test does raise division by zero if it failes
+ */
+BEGIN;
+
+-- Test if the function exists
+WITH test AS
+  (
+    SELECT COUNT(*) AS exist
+    FROM pg_catalog.pg_proc
+    WHERE proname = 'is_timestamp'
+  )
+SELECT 1 / test.exist AS res
+FROM test
+;
+
+-- Test with timestamp in default format
+WITH test AS
+	(
+		SELECT is_timestamp('2018-01-01 00:00:00') AS istimestamp, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN istimestamp THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+-- Test with wrong timestamp in default format
+WITH test AS
+	(
+		SELECT is_timestamp('2018-01-01 25:00:00') AS istimestamp, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN NOT istimestamp THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+-- Test with timestamp in German format
+WITH test AS
+	(
+		SELECT is_timestamp('01.01.2018 00:00:00', 'DD.MM.YYYY HH24.MI.SS') AS istimestamp, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN istimestamp THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+-- Test with wrong timestamp in German format
+WITH test AS
+	(
+		SELECT is_timestamp('01.01.2018 25:00:00', 'DD.MM.YYYY HH24.MI.SS') AS istimestamp, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN NOT istimestamp THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+ROLLBACK;
