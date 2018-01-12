984ddfd05fee063cdecbdb090f8642d72934ec7f
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-12 16:39:05 +0100

    Added comments and changed the format on the gap aggregate.

--- aggregate_gap.sql
+++ aggregate_gap.sql
@@ -1,14 +1,15 @@
--- Create a window function to calculate values for gaps
-CREATE OR REPLACE FUNCTION GapFillInternal(
-	s anyelement,
-	v anyelement
-)
+/**
+ * Create a window function to calculate values for gaps.
+ */
+CREATE OR REPLACE FUNCTION GapFillInternal(s anyelement, v anyelement)
 RETURNS anyelement AS
 $$
 BEGIN
 	RETURN COALESCE(v, s);
 END;
 $$ LANGUAGE PLPGSQL IMMUTABLE;
+COMMENT ON FUNCTION GapFillInternal(s anyelement, v anyelement) IS 'The function is used to fill gaps in window functions';
+
 
 -- The Window function needs an aggregate
 CREATE AGGREGATE GapFill(anyelement) (
@@ -16,3 +17,4 @@ CREATE AGGREGATE GapFill(anyelement) (
 	STYPE = anyelement
 )
 ;
+COMMENT ON AGGREGATE GapFill(anyelement) IS 'Implements the aggregate function to fill gaps using the function GapFillInternal';
