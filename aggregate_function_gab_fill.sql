767281428e6287dcd2af01343908eb07d43d9e9f
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-12 16:52:19 +0100

    Renamed the functions with underlines as separators for better readability.

--- aggregate_function_gab_fill.sql
+++ aggregate_function_gab_fill.sql
@@ -1,20 +1,21 @@
 /**
  * Create a window function to calculate values for gaps.
  */
-CREATE OR REPLACE FUNCTION GapFillInternal(s anyelement, v anyelement)
+CREATE OR REPLACE FUNCTION gap_fill_internal(s anyelement, v anyelement)
 RETURNS anyelement AS
 $$
 BEGIN
 	RETURN COALESCE(v, s);
 END;
 $$ LANGUAGE PLPGSQL IMMUTABLE;
-COMMENT ON FUNCTION GapFillInternal(s anyelement, v anyelement) IS 'The function is used to fill gaps in window functions';
+COMMENT ON FUNCTION gap_fill_internal(s anyelement, v anyelement) IS 'The function is used to fill gaps in window functions';
 
 
 -- The Window function needs an aggregate
-CREATE AGGREGATE GapFill(anyelement) (
-	SFUNC=GapFillInternal,
+DROP AGGREGATE gap_fill(anyelement) IF EXISTS;
+CREATE AGGREGATE gap_fill(anyelement) (
+	SFUNC=gap_fill_internal,
 	STYPE = anyelement
 )
 ;
-COMMENT ON AGGREGATE GapFill(anyelement) IS 'Implements the aggregate function to fill gaps using the function GapFillInternal';
+COMMENT ON AGGREGATE gap_fill(anyelement) IS 'Implements the aggregate function to fill gaps using the function GapFillInternal';
