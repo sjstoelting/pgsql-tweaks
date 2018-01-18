c409e15e483c938ea5d449b7b2d7ba314427065f
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-18 21:46:50 +0100

    Added tests on aggregate gab_fill.

--- tests/aggregate_function_gab_fill.sql
+++ tests/aggregate_function_gab_fill.sql
@@ -1,5 +1,5 @@
 /**
- * Test for the aggregate GapFill
+ * Test for the aggregate gap_fill
  *
  * Every test does raise division by zero if it failes
  */
@@ -8,7 +8,7 @@ BEGIN;
 -- Test if the function exists
 WITH test AS
   (
-    SELECT COUNT(*) AS exist
+    SELECT count(*) AS exist
     FROM pg_catalog.pg_proc
     WHERE proname = 'gap_fill_internal'
   )
@@ -19,7 +19,7 @@ FROM test
 -- Test if the aggregate exists
 WITH test AS
   (
-    SELECT COUNT(*) AS exist
+    SELECT count(*) AS exist
     FROM pg_catalog.pg_proc
     WHERE proname = 'gap_fill'
   )
@@ -27,5 +27,28 @@ SELECT 1 / test.exist AS res
 FROM test
 ;
 
+-- Create a table with some test values
+CREATE TABLE test_gap_fill(id INTEGER, some_value VARCHAR);
 
-END;
+INSERT INTO test_gap_fill(id, some_value) VALUES
+  (1, 'value 1'),
+  (1, NULL),
+  (2, 'value 2'),
+  (2, NULL),
+  (2, NULL),
+  (3, 'value 3')
+;
+
+-- Select the test data and return filled columns, the count of colums should be
+-- the same number as the count of not empty ones
+WITH t1 AS
+  (
+    SELECT id
+      , gap_fill(some_value) OVER (ORDER BY id) AS some_value
+    FROM test_gap_fill
+  )
+SELECT count(*) / count(*) FILTER (WHERE NOT some_value IS NULL) AS res
+FROM t1
+;
+
+ROLLBACK;
