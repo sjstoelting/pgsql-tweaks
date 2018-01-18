83c3f593bfdb7e19404f9751cdc5f7b916e32a3c
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-18 21:52:29 +0100

    Added functions to replace characters in a given encoding by other characters or empty strings. Tests are added, too.

---
+++ function_replace_encoding.sql
@@ -0,0 +1,87 @@
+/**
+ * Creates three function to replace characters, that are not part of the given
+ * encoding.
+ * The function does depend on the function is_encoding which is part of this
+ * repository.
+ */
+CREATE OR REPLACE FUNCTION replace_encoding(s VARCHAR, e VARCHAR) RETURNS VARCHAR AS $$
+DECLARE
+	i INTEGER := 0;
+	res VARCHAR;
+BEGIN
+	res := s;
+
+	LOOP
+		EXIT WHEN i > length(res);
+		i := i + 1;
+		IF (NOT is_encoding(substring(res FROM i FOR 1 ), e)) THEN
+			res := OVERLAY(res PLACING '' FROM i FOR 1);
+		END IF;
+	END LOOP;
+
+    RETURN res;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION replace_encoding(s VARCHAR, e VARCHAR) IS 'Replaces all characters, which are not part of the given encoding, with spaces and returns the result only with characters which are part of the given encoding';
+
+
+CREATE OR REPLACE FUNCTION replace_encoding(s VARCHAR, e VARCHAR, replacement VARCHAR) RETURNS VARCHAR AS $$
+DECLARE
+	i INTEGER := 0;
+	res VARCHAR;
+BEGIN
+	res := s;
+
+	LOOP
+		EXIT WHEN i > length(res);
+		i := i + 1;
+		IF (NOT is_encoding(substring(res FROM i FOR 1 ), e)) THEN
+			res := OVERLAY(res PLACING replacement FROM i FOR 1);
+		END IF;
+	END LOOP;
+
+    RETURN res;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION replace_encoding(s VARCHAR, e VARCHAR, replacement VARCHAR) IS 'Replaces all characters, which are not part of the given encoding, with the given replacement in the third parameter and returns the result only with characters which are part of the given encoding';
+
+
+CREATE OR REPLACE FUNCTION replace_encoding(s VARCHAR, s_search VARCHAR[], s_replace VARCHAR[]) RETURNS VARCHAR AS $$
+DECLARE
+	i INTEGER := 0;
+	res VARCHAR;
+	length_equal BOOLEAN;
+	a_count INTEGER;
+BEGIN
+
+	SELECT array_length(s_search, 1) = array_length(s_replace, 1) INTO length_equal;
+
+	IF NOT length_equal THEN
+		RAISE 'Search and replacement arrays do not have the same count of entries' USING ERRCODE = '22000';
+	END IF;
+
+	SELECT array_length(s_search, 1) INTO a_count;
+	res := s;
+
+	LOOP
+		EXIT WHEN i >= a_count;
+		i := i + 1;
+
+		res := REPLACE(res, s_search[i], s_replace[i]);
+
+	END LOOP;
+
+  RETURN res;
+
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION replace_encoding(s VARCHAR, s_search VARCHAR[], s_replace VARCHAR[]) IS 'Replaces charactes given in s_search with characters given in s_replace at the same array position';
---
+++ tests/function_replace_encoding.sql
@@ -0,0 +1,112 @@
+/**
+ * Test for the functions replace_encoding
+ *
+ * Every test does raise division by zero if it failes
+ */
+BEGIN;
+
+-- Test if the function exists
+WITH test AS
+  (
+    SELECT count(*) AS exist
+    FROM pg_catalog.pg_proc
+    WHERE proname = 'replace_encoding'
+  )
+SELECT 1 / test.exist  AS res
+FROM test
+;
+
+-- Test if all three implementations exists
+WITH test AS
+  (
+    SELECT count(*) AS exist
+      , 0 AS zero
+    FROM pg_catalog.pg_proc
+    WHERE proname = 'replace_encoding'
+  )
+SELECT
+  CASE
+    WHEN test.exist = 3 THEN
+      1
+    ELSE
+      1 / test.zero
+  END AS res
+FROM test
+;
+
+-- Test of the first implementation which replaces none latin1 characters with
+-- empty strings
+WITH test AS
+	(
+		SELECT 'ağbƵcğeƵ' AS test_string
+      , 'latin1' AS enc
+	    , 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN length(test_string) = 8 AND length(replace_encoding(test_string, enc)) = 4 THEN
+			1
+		ELSE
+			1 / zero
+  END AS res_1
+  , CASE
+		WHEN is_encoding(replace_encoding(test_string, enc), enc) THEN
+			1
+		ELSE
+			1 / zero
+	END AS res_2
+FROM test
+;
+
+-- Test of the second implementation which replaces none latin1 characters with
+-- second parameter
+WITH test AS
+	(
+		SELECT 'ağbcğe' AS test_string
+      , 'latin1' AS enc
+			, 'g' AS replacement
+			, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN length(test_string) = length(replace_encoding(test_string, enc, replacement)) THEN
+			1
+		ELSE
+			1 / zero
+	END AS res_1
+  , CASE
+		WHEN is_encoding(replace_encoding(test_string, enc, replacement), enc) THEN
+			1
+		ELSE
+			1 / zero
+	END AS res_2
+FROM test
+;
+
+-- Test of the third implementation which replaces given none latin1 characters
+-- in an array as second parameter with latin1 characters given in an array as
+-- the third paramater
+WITH test AS
+	(
+		SELECT 'ağbƵcğeƵ' AS test_string
+			, string_to_array('ğ,Ƶ', ',') AS to_replace
+			, string_to_array('g,Z', ',') AS replacement
+			, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN length(test_string) = length(replace_encoding(test_string, to_replace, replacement)) THEN
+			1
+		ELSE
+			1 / zero
+	END AS res_1
+	, CASE
+		WHEN is_latin1(replace_encoding(test_string, to_replace, replacement)) THEN
+			1
+		ELSE
+			1 / zero
+	END AS res_2
+FROM test
+;
+
+ROLLBACK;
