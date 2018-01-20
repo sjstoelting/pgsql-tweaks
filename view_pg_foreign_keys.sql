6b3f828cc3263e21fbabca9671ea7f991f933980
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-01-20 17:31:03 +0100

    Changed columns to lower case, just some code format stuff.

--- view_pg_foreign_keys.sql
+++ view_pg_foreign_keys.sql
@@ -1,18 +1,18 @@
 /**
  * Creates a view to get all foreign keys of the current database.
  */
-CREATE OR REPLACE VIEW pg_foreign_keys AS
-SELECT tc.TABLE_CATALOG
-	, tc.TABLE_SCHEMA
-  , tc.TABLE_NAME
-  , kcu.COLUMN_NAME
-  , ccu.TABLE_NAME AS foreign_table_name
-  , ccu.COLUMN_NAME AS foreign_column_name
-FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS tc
-  INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS kcu
-  	ON tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME
-  INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE AS ccu
-  	ON ccu.CONSTRAINT_NAME = tc.CONSTRAINT_NAME
-WHERE tc.CONSTRAINT_TYPE = 'FOREIGN KEY'
-;
-COMMENT ON VIEW pg_foreign_keys IS 'The view returns all foreign keys of the current database'
+ CREATE OR REPLACE VIEW pg_foreign_keys AS
+ SELECT tc.table_catalog
+ 	, tc.table_schema
+ 	, tc.table_name
+ 	, kcu.column_name
+ 	, ccu.TABLE_NAME AS foreign_table_name
+ 	, ccu.COLUMN_NAME AS foreign_column_name
+ FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS tc
+ 	INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS kcu
+ 		ON tc.CONSTRAINT_NAME = kcu.CONSTRAINT_NAME
+ 		INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE AS ccu
+ 			ON ccu.CONSTRAINT_NAME = tc.CONSTRAINT_NAME
+ WHERE tc.CONSTRAINT_TYPE = 'FOREIGN KEY'
+ ;
+ COMMENT ON VIEW pg_foreign_keys IS 'The view returns all foreign keys of the current database'
