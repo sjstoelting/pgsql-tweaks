9b5aebebfa8ec260e9ee36995f1ad9cbe0985637
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-02-26 21:45:31 +0100

    Added missing numeric data types REAL, DOUBLE PRECISION, and NUMERIC to array_avg() function. Tests for the extended functionality have been added, too.

--- sql/function_array_avg.sql
+++ sql/function_array_avg.sql
@@ -2,14 +2,11 @@
  * Returns the average value of an array.
  * Implementation for BIGINT, INTEGER, SMALLINT
  */
--- The result type has been changed, therefore existing functins have to be dropped
-DROP FUNCTION IF EXISTS array_avg(a BIGINT[]);
-DROP FUNCTION IF EXISTS array_avg(a INTEGER[]);
-DROP FUNCTION IF EXISTS array_avg(a SMALLINT[]);
 
-CREATE OR REPLACE FUNCTION array_avg(a BIGINT[]) RETURNS DOUBLE PRECISION AS $$
+-- BIGINT implementation
+CREATE OR REPLACE FUNCTION array_avg(a BIGINT[]) RETURNS NUMERIC AS $$
 DECLARE
-	res DOUBLE PRECISION;
+	res NUMERIC;
 BEGIN
 
 	WITH unnested AS
@@ -25,11 +22,12 @@ $$
 STRICT
 LANGUAGE plpgsql IMMUTABLE
 ;
-COMMENT ON FUNCTION array_avg(a BIGINT[]) IS 'Returns the average value of an array';
+COMMENT ON FUNCTION array_avg(a BIGINT[]) IS 'Returns the average value of a BIGINT array';
 
-CREATE OR REPLACE FUNCTION array_avg(a INTEGER[]) RETURNS DOUBLE PRECISION AS $$
+-- INTEGER implementation
+CREATE OR REPLACE FUNCTION array_avg(a INTEGER[]) RETURNS NUMERIC AS $$
 DECLARE
-	res DOUBLE PRECISION;
+	res NUMERIC;
 BEGIN
 
 	WITH unnested AS
@@ -45,11 +43,12 @@ $$
 STRICT
 LANGUAGE plpgsql IMMUTABLE
 ;
-COMMENT ON FUNCTION array_avg(a INTEGER[]) IS 'Returns the average value of an array';
+COMMENT ON FUNCTION array_avg(a INTEGER[]) IS 'Returns the average value of an INTEGER array';
 
-CREATE OR REPLACE FUNCTION array_avg(a SMALLINT[]) RETURNS DOUBLE PRECISION AS $$
+-- SMALLINT implementation
+CREATE OR REPLACE FUNCTION array_avg(a SMALLINT[]) RETURNS NUMERIC AS $$
 DECLARE
-	res DOUBLE PRECISION;
+	res NUMERIC;
 BEGIN
 
 	WITH unnested AS
@@ -65,4 +64,67 @@ $$
 STRICT
 LANGUAGE plpgsql IMMUTABLE
 ;
-COMMENT ON FUNCTION array_avg(a SMALLINT[]) IS 'Returns the average value of an array';
+COMMENT ON FUNCTION array_avg(a SMALLINT[]) IS 'Returns the average value of a SMALLINT array';
+
+--REAL implementation
+CREATE OR REPLACE FUNCTION array_avg(a REAL[]) RETURNS NUMERIC AS $$
+DECLARE
+	res NUMERIC;
+BEGIN
+
+	WITH unnested AS
+ 		(
+			SELECT UNNEST(a) AS vals
+		)
+		SELECT avg(vals) FROM unnested AS x INTO res;
+
+	RETURN res;
+
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION array_avg(a REAL[]) IS 'Returns the average value of a REAL array';
+
+-- DOUBLE PRECISION implementation
+CREATE OR REPLACE FUNCTION array_avg(a DOUBLE PRECISION[]) RETURNS NUMERIC AS $$
+DECLARE
+	res NUMERIC;
+BEGIN
+
+	WITH unnested AS
+ 		(
+			SELECT UNNEST(a) AS vals
+		)
+		SELECT avg(vals) FROM unnested AS x INTO res;
+
+	RETURN res;
+
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION array_avg(a DOUBLE PRECISION[]) IS 'Returns the average value of a DOUBLE PRECISION array';
+
+-- NUMERIC implementation
+CREATE OR REPLACE FUNCTION array_avg(a NUMERIC[]) RETURNS NUMERIC AS $$
+DECLARE
+	res NUMERIC;
+BEGIN
+
+	WITH unnested AS
+ 		(
+			SELECT UNNEST(a) AS vals
+		)
+		SELECT avg(vals) FROM unnested AS x INTO res;
+
+	RETURN res;
+
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION array_avg(a NUMERIC[]) IS 'Returns the average value of a NUMERIC array';
--- test/sql/function_array_avg.sql
+++ test/sql/function_array_avg.sql
@@ -12,7 +12,7 @@ WITH test AS
 		FROM pg_catalog.pg_proc
 		WHERE proname = 'array_avg'
   )
-SELECT 3 / test.exist = 1 AS res
+SELECT 6 / test.exist = 1 AS res
 FROM test
 ;
 
@@ -26,7 +26,7 @@ WITH test AS
   )
 SELECT
 	CASE
-    	WHEN test.exist = 3 THEN
+    	WHEN test.exist = 6 THEN
 			TRUE
 		ELSE
 			(1 / test.zero)::BOOLEAN
@@ -78,6 +78,93 @@ WITH test AS
 SELECT
 	CASE
 		WHEN avg_value = 61.75 THEN
+			TRUE
+		ELSE
+			(1 / test.zero)::BOOLEAN
+  END AS res_1
+FROM test
+;
+
+-- Test of the fourth implementation
+-- REAL ARRAY
+WITH test_data AS
+	(
+		SELECT 45.6::REAL AS val
+		UNION ALL
+		SELECT 60.8::REAL
+		UNION ALL
+		SELECT 43::REAL
+		UNION ALL
+		SELECT 99.3::REAL
+	)
+, test AS
+	(
+		SELECT array_avg(ARRAY[45.6, 60.8, 43, 99.3]::REAL[]) AS avg_value
+	    	, 0 AS zero
+			, avg(val) AS test_val
+		FROM test_data
+	)
+SELECT
+	CASE
+		WHEN avg_value::NUMERIC = test_val::NUMERIC THEN
+			TRUE
+		ELSE
+			(1 / test.zero)::BOOLEAN
+  END AS res_1
+FROM test
+;
+
+-- Test of the fifth implementation
+-- DOUBLE PRECISION ARRAY
+WITH test_data AS
+	(
+		SELECT 45.6::DOUBLE PRECISION AS val
+		UNION ALL
+		SELECT 60.8::DOUBLE PRECISION
+		UNION ALL
+		SELECT 43::DOUBLE PRECISION
+		UNION ALL
+		SELECT 99.3::DOUBLE PRECISION
+	)
+, test AS
+	(
+		SELECT array_avg(ARRAY[45.6, 60.8, 43, 99.3]::DOUBLE PRECISION[]) AS avg_value
+		    , 0 AS zero
+			, avg(val) AS test_val
+			FROM test_data
+	)
+SELECT
+	CASE
+		WHEN avg_value::NUMERIC = test_val::NUMERIC THEN
+			TRUE
+		ELSE
+			(1 / test.zero)::BOOLEAN
+  END AS res_1
+FROM test
+;
+
+-- Test of the sicth implementation
+-- NUMERIC ARRAY
+WITH test_data AS
+	(
+		SELECT 45.6::NUMERIC AS val
+		UNION ALL
+		SELECT 60.8::NUMERIC
+		UNION ALL
+		SELECT 43::NUMERIC
+		UNION ALL
+		SELECT 99.3::NUMERIC
+	)
+, test AS
+	(
+		SELECT array_avg(ARRAY[45.6, 60.8, 43, 99.3]::NUMERIC[]) AS avg_value
+		    , 0 AS zero
+			, avg(val) AS test_val
+			FROM test_data
+	)
+SELECT
+	CASE
+		WHEN avg_value::NUMERIC = test_val::NUMERIC THEN
 			TRUE
 		ELSE
 			(1 / test.zero)::BOOLEAN
