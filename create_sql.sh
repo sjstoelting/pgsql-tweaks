64a9c2bac9c3b91f3d8c82f8711075f8b9b41037
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-03-05 16:40:08 +0100

    Moved the location of the information in the creation script to the top position of the SQL installation file.

--- create_sql.sh
+++ create_sql.sh
@@ -16,16 +16,6 @@ FILENAME="$DIR/$EXTENSION""_uninstall--$EXTVERSION.sql"
 # Always start with an empty file
 truncate -s 0 $FILENAME
 
-# Add licencse information
-echo '/**' >> $FILENAME
-echo ' * PostgreSQL pgsql_tweaks extension' >> $FILENAME
-echo ' * Licence:    PostgreSQL Licence, see https://raw.githubusercontent.com/sjstoelting/pgsql-tweaks/master/LICENSE.md' >> $FILENAME
-echo ' * Author:     Stefanie Janine Stölting <mail@stefanie-stoelting.de>' >> $FILENAME
-echo ' * Repository: http://github.com/sjstoelting/pgsql_tweaks/' >> $FILENAME
-echo ' * Version:    '$EXTVERSION >> $FILENAME
-echo ' */' >> $FILENAME
-
-echo '' >> $FILENAME
 echo '/*** uninstall file to drop all objects created by the extension pgsql_tweaks ***/' >> $FILENAME
 echo '' >> $FILENAME
 
@@ -144,6 +134,15 @@ arraylength=${#SQLFILES[@]}
 
 # Always start with an empty file
 truncate -s 0 $FILENAME
+
+# Add licencse information
+echo '/**' >> $FILENAME
+echo ' * PostgreSQL pgsql_tweaks extension' >> $FILENAME
+echo ' * Licence:    PostgreSQL Licence, see https://raw.githubusercontent.com/sjstoelting/pgsql-tweaks/master/LICENSE.md' >> $FILENAME
+echo ' * Author:     Stefanie Janine Stölting <mail@stefanie-stoelting.de>' >> $FILENAME
+echo ' * Repository: http://github.com/sjstoelting/pgsql_tweaks/' >> $FILENAME
+echo ' * Version:    '$EXTVERSION >> $FILENAME
+echo ' */' >> $FILENAME
 
 # Add initial statements
 echo '' >> $FILENAME
