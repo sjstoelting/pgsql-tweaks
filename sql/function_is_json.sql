343cbf7bfcbefd9bfebb5748cd3acc046f9e3477
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-06-28 11:40:58 +0200

    Added a function to check, if a given text is a JSON object, tests have been added, too.

---
+++ sql/function_is_json.sql
@@ -0,0 +1,15 @@
+/**
+ * Creates a function to check strings for being INTEGER.
+ */
+CREATE OR REPLACE FUNCTION is_json(s VARCHAR) RETURNS BOOLEAN AS $$
+BEGIN
+	PERFORM s::JSON;
+	RETURN TRUE;
+EXCEPTION WHEN others THEN
+	RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE
+;
+COMMENT ON FUNCTION is_json(s VARCHAR) IS 'Checks, whether the given text is a JSON';
---
+++ test/sql/function_is_json.sql
@@ -0,0 +1,49 @@
+/**
+ * Test for function is_json
+ *
+ * Every test does raise division by zero if it failes
+ */
+BEGIN;
+
+-- Test if the function exists
+WITH test AS
+	(
+		SELECT COUNT(*) AS exist
+		FROM pg_catalog.pg_proc
+		WHERE proname = 'is_json'
+	)
+SELECT 1 / test.exist = 1 AS res
+FROM test
+;
+
+-- Test with a test string containing no JSON
+WITH test AS
+	(
+		SELECT is_json('Not a JSON') AS isjson, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN NOT isjson THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+-- Test with a test string containing JSON
+WITH test AS
+	(
+		SELECT is_json('{"review": {"date": "1970-12-30", "votes": 10, "rating": 5, "helpful_votes": 0}, "product": {"id": "1551803542", "group": "Book", "title": "Start and Run a Coffee Bar (Start & Run a)", "category": "Business & Investing", "sales_rank": 11611, "similar_ids": ["0471136174", "0910627312", "047112138X", "0786883561", "0201570483"], "subcategory": "General"}, "customer_id": "AE22YDHSBFYIP"}') AS isjson, 0 AS zero
+	)
+SELECT
+	CASE
+		WHEN isjson THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+ROLLBACK;
