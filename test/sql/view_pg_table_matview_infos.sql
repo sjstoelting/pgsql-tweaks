4dcc432c2981336c2308edde0962f02e221c5d7f
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-11-06 14:22:54 +0100

    Added tests on the view view_pg_table_matview_info.

---
+++ test/sql/view_pg_table_matview_infos.sql
@@ -0,0 +1,36 @@
+/**
+ * Test for view pg_table_matview_infos
+ *
+ * Every test does raise division by zero if it failes
+ */
+BEGIN;
+
+-- Test if the function exists
+WITH test AS
+	(
+		SELECT COUNT(*) AS exist
+		FROM pg_catalog.pg_views
+		WHERE viewname = 'pg_table_matview_infos'
+	)
+SELECT 1 / test.exist = 1 AS res
+FROM test
+;
+
+-- Test if the view runs without errors
+WITH test AS
+	(
+	SELECT count(*) as key_count
+			, 0 AS zero
+	FROM pg_table_matview_infos
+	)
+SELECT
+	CASE
+		WHEN key_count >= 0 THEN
+			TRUE
+		ELSE
+			(1 / zero)::BOOLEAN
+	END AS res
+FROM test
+;
+
+ROLLBACK;
