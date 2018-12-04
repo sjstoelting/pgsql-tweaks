8c495f9dae7f7437b3d118945425e100554966f4
Stefanie Janine Stoelting <mail@stefanie-stoelting.de>
2018-12-04 22:31:38 +0100

    Changed the view pg_functions: There are two versions needed, one for PostgreSQL 11 and newer and one for PostgreSQL 10 or older.

--- sql/view_pg_functions-10.sql
+++ sql/view_pg_functions-10.sql
@@ -1,6 +1,6 @@
 /**
  * Creates a view to get all functions of the current database.
- * This is the script for PostgreSQL 11 or newer
+ * This is the script for PostgreSQL 10 or older
  */
 CREATE OR REPLACE VIEW pg_functions AS
 SELECT pg_namespace.nspname AS schema_name
@@ -8,16 +8,14 @@ SELECT pg_namespace.nspname AS schema_name
 	, pg_catalog.pg_get_function_result(pg_proc.oid) AS returning_data_type
 	, pg_catalog.pg_get_function_arguments(pg_proc.oid) AS parameters
 	, CASE
-			WHEN pg_proc.prokind = 'a' THEN
+			WHEN pg_proc.proisagg THEN
 				'aggregate'
-			WHEN pg_proc.prokind = 'w' THEN
+			WHEN pg_proc.proiswindow THEN
 				'window'
-			WHEN pg_proc.prokind = 'f' THEN
-				'function'
-			WHEN pg_proc.prorettype = 'pg_catalog.trigger'::pg_catalog.regtype THEN
-				'trigger'
-			ELSE
-				'unknown'
+			 WHEN pg_proc.prorettype = 'pg_catalog.trigger'::pg_catalog.regtype THEN
+			 	'trigger'
+			 ELSE
+			 	'function'
 		END as function_type
 	, pg_description.description AS function_comment
 FROM pg_catalog.pg_proc
