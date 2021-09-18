2055edce820740dc84a46ed0761b7372e7e675ee
Stefanie Janine Stölting <mail@stefanie-stoelting.de>
2021-09-18 15:11:43 +0200

    Added version checking to the view pg_partitioned_tables_infos and its test.

--- test/sql/view_pg_partitioned_tables_infos.sql
+++ test/sql/view_pg_partitioned_tables_infos.sql
@@ -3,41 +3,53 @@
  *
  * Every test does raise division by zero if it failes
  */
-BEGIN;
 
--- Test if the function exists
-WITH test AS
-	(
-		SELECT COUNT(*) AS exist
-			, 0 AS zero
-		FROM pg_catalog.pg_views
-		WHERE viewname = 'pg_partitioned_tables_infos'
-	)
-SELECT
-	CASE
-		WHEN 1 / test.exist = 1 THEN
-			TRUE
-		ELSE
-			(1 / zero)::BOOLEAN
-	END AS res
-FROM test
-;
+DO $$
+DECLARE
+	version_greater_11 BOOLEAN;
+	res BOOLEAN;
+BEGIN
+	SELECT to_number((string_to_array(version(), ' '))[2], '999.99') >= 11 INTO version_greater_11;
 
--- Test if the view runs without errors
-WITH test AS
-	(
-	SELECT count(*) as key_count
-			, 0 AS zero
-	FROM pg_partitioned_tables_infos
-	)
-SELECT
-	CASE
-		WHEN key_count >= 0 THEN
-			TRUE
-		ELSE
-			(1 / zero)::BOOLEAN
-	END AS res
-FROM test
-;
+	IF version_greater_11 THEN
+		-- Test if the function exists
+		WITH test AS
+			(
+				SELECT COUNT(*) AS exist
+					, 0 AS zero
+				FROM pg_catalog.pg_views
+				WHERE viewname = 'pg_partitioned_tables_infos'
+			)
+		SELECT
+			CASE
+				WHEN 1 / test.exist = 1 THEN
+					TRUE
+				ELSE
+					(1 / zero)::BOOLEAN
+			END AS res
+		FROM test
+		INTO res
+		;
 
-ROLLBACK;
+		-- Test if the view runs without errors
+		WITH test AS
+			(
+			SELECT count(*) as key_count
+					, 0 AS zero
+			FROM pg_partitioned_tables_infos
+			)
+		SELECT
+			CASE
+				WHEN key_count >= 0 THEN
+					TRUE
+				ELSE
+					(1 / zero)::BOOLEAN
+			END AS res
+		FROM test
+		INTO res
+		;
+
+		ROLLBACK;
+	END IF;
+
+END $$;
